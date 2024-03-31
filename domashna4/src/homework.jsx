c2521591809a5d203b8e3e2348c1cf036aba6b26
ketibm <k.matenicharska@outlook.com>
2024-03-31 17:32:23 +0200

    domashna4

---
+++ domashna4/src/homework.jsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import './App.css'
+import './homework.css'
+
+function Homework()  {
+    const [data, setData] = useState({
+        name: '',
+        lastName: '',
+        phoneNumber: '',
+        yearOfBirth: '',
+    })
+
+    const [flag, setFlag] = useState(false)
+    const [formSubmitted, setFormSubmitted] = useState(false);
+    const [date, setDate] = useState();
+
+    useEffect(() => {
+        if (formSubmitted) {
+            const isEmpty = Object.values(data).some(value => value === '');
+            if (isEmpty) {
+                alert('Alert: All fields are required!');
+                setFormSubmitted(false);
+        }
+    }
+    }, [formSubmitted]);
+    
+    const handleSubmit = (e) => {
+        e.preventDefault(); 
+        setFormSubmitted(true);
+        const isEmpty = Object.values(data).some(value => value === '');
+        if (!isEmpty) {
+            setFlag(true);
+        }
+    };
+
+    const handleChange = (value, inputName) => {
+        setData({ ...data, [inputName]: value.target.value })
+    }
+   
+    const handleDateChange = (date) => {
+        setDate(date);
+        const year = date.getFullYear(); 
+        setData({ ...data, yearOfBirth: year.toString() }); 
+    };
+
+    return (
+    <div>
+    {flag ? (
+        <div className="card">
+            <div className="header">
+                <img src="https://icons.veryicon.com/png/o/miscellaneous/micro-credit/personal-data-27.png" weight="40px" height="20px"/>
+                <h2>User data</h2>
+            </div>
+            <p>Name: {data.name}</p>
+            <p>Last Name: {data.lastName}</p>
+            <p>Phone Number: {data.phoneNumber}</p>
+            <p>Year of Birth: {data.yearOfBirth}</p>
+        </div>
+    ) : (
+        <form onSubmit={handleSubmit}>
+            <input onChange={(e) => handleChange(e, 'name')} value={data.name} type='text' placeholder='Name' />
+            <br />
+            <input onChange={(e) => handleChange(e, 'lastName')} value={data.lastName} type='text' placeholder='Last Name' />
+            <br />
+            <input onChange={(e) => handleChange(e, 'phoneNumber')} value={data.phoneNumber} type='number' placeholder='Phone Number' />
+            <br />
+            <DatePicker selected={date} onChange={handleDateChange} dateFormat="yyyy" showYearPicker placeholderText='Year of Birth' />
+            <br />
+            <button>Submit</button>
+        </form>
+    )}
+</div>
+    )
+}
+
+export default Homework
---
+++ domashna4/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import Homework from './homework.jsx'
+import './index.css'
+import './homework.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Homework />
+  </React.StrictMode>,
+)
